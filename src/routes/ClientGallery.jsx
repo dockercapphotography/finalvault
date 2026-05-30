ce24e4f450c7b3ab0981ae567501c11ef9b3a259
Nick Porterfield <276164349+nickporterfieldfullsteam@users.noreply.github.com>
2026-05-30 18:05:02 -0400

    Add PWA manifest, replace all inline logo SVGs with finalvault_logo.svg

--- src/routes/ClientGallery.jsx
+++ src/routes/ClientGallery.jsx
@@ -81,17 +81,7 @@ function GateWrapper({ gallery, photographerName, children }) {
 
       {/* Footer */}
       <div className="relative z-10 mt-8 text-center flex flex-col items-center gap-1.5">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 532.02 542.02" width="20" height="20" style={{ opacity: 0.4 }}>
-          <path fill="#ebdcfa" d="M439.34,65.62v132.41l-101.98-103.41L246.39.76c70.46-5.44,140.07,18.4,192.95,64.86Z"/>
-          <path fill="#f7effe" d="M531.32,251.43l-91.96,92.14V65.62c53.83,47.29,86.78,113.86,91.96,185.8Z"/>
-          <path fill="#481a7a" d="M467.34,447.67h-129.32l101.39-104.11,91.91-92.14c5.12,71.11-17.86,142.13-63.98,196.25Z"/>
-          <path fill="#6731a1" d="M338.02,447.58h129.32c-46.24,54.27-110.69,88.11-181.9,93.66l-90.98-93.66h143.56Z"/>
-          <path fill="#974ae7" d="M194.46,447.39l90.98,93.85c-70.28,5.48-139.92-18.33-192.72-64.81v-132.33l101.73,103.29Z"/>
-          <path fill="#a766eb" d="M92.77,344.1v132.33C38.76,428.88,5.94,362.55.72,290.58l92.05-92.62v146.14Z"/>
-          <path fill="#b780ef" d="M194.01,94.75l-101.15,103.21L.72,290.58c-5.14-70.88,17.47-141.47,63.16-195.83h130.13Z"/>
-          <path fill="#d7b8f6" d="M246.39.76l90.97,93.95H63.88C109.81,40.07,174.89,6.28,246.39.76Z"/>
-          <path fill="#b780ef" d="M317.66,398.93h-103.13l-73.8-74.73v-106.2l73.84-74.95h103.61l73.07,75.08v106.04l-73.6,74.76ZM296.82,344.99l42.12-43.56v-61.2l-43.33-44.16h-59.18l-43.35,43.39v62.03c13.74,15.79,28.37,29.02,42.59,43.51h61.15Z"/>
-        </svg>
+        <img src="/finalvault_logo.svg" alt="FinalVault" width="20" height="20" style={{ opacity: 0.4 }} />
         <span style={{
           fontFamily: "'Montserrat', sans-serif",
           fontWeight: 700,
